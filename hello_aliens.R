0f8d431e6a6f897bdc1bde12f98c4e4ab7ec035f
MacBook Pro <jl@MacBookdeMacBook-Pro.local>
2017-12-10 17:14:58 -0500

    350 final project Q20

---
+++ hello_aliens.R
@@ -0,0 +1,4 @@
+cat("Hello aliens. This is a letter from the Earth.
+The Erath Earth is the third planet from the Sun and formed over 4 billion years ago.
+Earth orbits around the Sun in 365.26 days, and we call this period an Earth year. 
+Can you tell me something about your planet?")
